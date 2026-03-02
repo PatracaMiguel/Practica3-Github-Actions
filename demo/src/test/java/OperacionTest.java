9591c5fdf79098878f61e56c7f92fa6087a8a1f0
Miguel <miguelangelpatracalagunes@gmail.com>
2026-03-02 13:41:23 -0600

    Correccion de rutas 2

--- demo/src/test/java/OperacionTest.java
+++ demo/src/test/java/OperacionTest.java
@@ -3,34 +3,43 @@ import org.junit.jupiter.api.Test;
 import Matematicas.Operacion;
 
 public class OperacionTest {
+    int pruebas = 0;
+    int pruebasTotales = 5;
     
     @Test
     public void testSumar() {
         Operacion operacion = new Operacion(5, 3);
         assertEquals(8, operacion.sumar());
+        pruebas++;
     }
 
     @Test
     public void testRestar() {
         Operacion operacion = new Operacion(5, 3);
         assertEquals(2, operacion.restar());
+        pruebas++;
     }
 
     @Test
     public void testMultiplicar() {
         Operacion operacion = new Operacion(5, 3);
         assertEquals(15, operacion.multiplicar());
+        pruebas++;
     }
 
     @Test
     public void testDividir() {
         Operacion operacion = new Operacion(10, 2);
         assertEquals(5, operacion.dividir());
+        pruebas++;
     }
 
-    @Test
-    public void testDividirPorCero() {
-        Operacion operacion = new Operacion(10, 0);
-        assertThrows(ArithmeticException.class, () -> operacion.dividir());
-    }
+        @Test
+        public void testDividirPorCero() {
+            Operacion operacion = new Operacion(10, 0);
+            assertThrows(ArithmeticException.class, () -> operacion.dividir());
+            pruebas++;
+        }
+
+    
 }
